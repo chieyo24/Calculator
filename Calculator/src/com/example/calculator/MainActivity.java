6aee41791158decc105447dd18745d89a5000a5a
chieyo_chen <chieyo_chen@askey.com.tw>
2015-07-24 18:11:17 +0800

    modify for debug error

--- Calculator/src/com/example/calculator/MainActivity.java
+++ Calculator/src/com/example/calculator/MainActivity.java
@@ -6,141 +6,107 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.example.calculator.R;
-
 public class MainActivity extends Activity {
 
-    private StringBuffer numberStr = null;
-    private Character operator = null;
-    private double num = 0.0;
-    private double numtemp = 0.0;
-    private EditText showResult=null;
+    private EditText mShowResultEdt;
+
+    private StringBuffer mNumberBufStr = null;
+    private Character mOperator = null;
+    private double mNum = 0.0;
+    private double mPreOperands = 0.0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        showResult = (EditText) findViewById(R.id.result_id);
-        
-        //initial
-        numberStr = new StringBuffer();
-        reset();
-    }
-    
-    public void btn0Clicked(View v) {
-        insert('0');
-    }
-    
-    public void btn1Clicked(View v) {
-        insert('1');
+        mShowResultEdt = (EditText) findViewById(R.id.result_id);
+
+        // initial
+        mNumberBufStr = new StringBuffer();
+        doDataReset();
     }
 
-    public void btn2Clicked(View v) {
-        insert('2');
+    /**
+     * Button OnClickListener do number button click logic
+     */
+    public void doNumberBtnClicked(View v) {
+        Button btn = (Button) v;
+        Character c = btn.getText().charAt(0);
+
+        if (c == '.' && mNumberBufStr.length() == 0)
+            return;
+
+        mNumberBufStr.append(c);
+        mNum = Double.valueOf(mNumberBufStr.toString()).doubleValue();
+        mShowResultEdt.setText(mNumberBufStr);
     }
 
-    public void btn3Clicked(View v) {
-        insert('3');
+    /**
+     * calculator
+     */
+    private double calculator(double preOperands, Character operator, double postOperands) {
+        double ans = 0.0;
+        if (operator == '+') {
+            ans = preOperands + postOperands;
+        } else if (operator == '-') {
+            ans = preOperands - postOperands;
+        } else if (operator == '*') {
+            ans = preOperands * postOperands;
+        } else if (operator == '/') {
+            ans = preOperands / postOperands;
+        }
+        return ans;
     }
 
-    public void btn4Clicked(View v) {
-        insert('4');
+    /**
+     * Button OnClickListener do Operator button click logic
+     */
+    public void doOperatorBtnClicked(View v) {
+        if (mPreOperands != 0.0 && mPreOperands != 'o' && mNum != 0.0) {
+            mNum = calculator(mPreOperands, mOperator, mNum);
+        }
+        Button btn = (Button) v;
+        this.mOperator = btn.getText().charAt(0);
+        mPreOperands = mNum;
+        mNumberBufStr.delete(0, mNumberBufStr.length());
     }
 
-    public void btn5Clicked(View v) {
-        insert('5');
+    /**
+     * Button OnClickListener do Operator button click logic
+     */
+    public void doEqualBtnClicked(View v) {
+        mNum = calculator(mPreOperands, mOperator, mNum);
+        String ans = String.valueOf(mNum).replace(".0", "");
+        mOperator = 'o';
+        mNumberBufStr.delete(0, mNumberBufStr.length());
+        mShowResultEdt.setText("" + ans);
     }
 
-    public void btn6Clicked(View v) {
-        insert('6');
+    /**
+     * Button OnClickListener do clear button click logic
+     */
+    public void doClearBtnClicked(View v) {
+        doDataReset();
     }
 
-    public void btn7Clicked(View v) {
-        insert('7');
-    }
-
-    public void btn8Clicked(View v) {
-        insert('8');
-    }
-
-    public void btn9Clicked(View v) {
-        insert('9');
-    }
-    
-    public void btnfloatClicked(View v) {
-        insert('.');
-    }
-
-    public void btnplusClicked(View v) {
-        perform('+');
-    }
-
-    public void btnminusClicked(View v) {
-        perform('-');
-    }
-    
-    public void btnmultiClicked(View v) {
-        perform('*');
-    }
-
-    public void btndivideClicked(View v) {
-        perform('/');
-    }
-
-
-    public void btnequalClicked(View v) {
-        calculate();
-    }
-
-    public void btnclearClicked(View v) {
-        reset();
-    }
-    
-    public void btnbkClicked(View v) {
-        backspace();
-    }
-
-    private void reset() {
-        numberStr.delete(0, numberStr.length());
-        operator = 'o';
-        num = 0.0;
-        numtemp = 0.0;
-        showResult.setText("");
-    }
-    
-    private void backspace() {
-        if(numberStr.length()>0){
-//            numberStr = numberStr.substring(0, numberStr.length()-1);
-            numberStr.delete(numberStr.length()-1, numberStr.length());
-            showResult.setText(numberStr);
+    /**
+     * Button OnClickListener do backspace button click logic
+     */
+    public void doBackspaceBtnClicked(View v) {
+        if (mNumberBufStr.length() > 0) {
+            mNumberBufStr.delete(mNumberBufStr.length() - 1, mNumberBufStr.length());
+            mShowResultEdt.setText(mNumberBufStr);
         }
     }
 
-    private void insert(Character numberChar) {
-//        numberStr = numberStr + numberChar;
-        numberStr.append(numberChar);
-        num = Double.valueOf(numberStr.toString()).doubleValue();
-        showResult.setText(numberStr);
+    /**
+     * Initial data
+     */
+    private void doDataReset() {
+        mNumberBufStr.delete(0, mNumberBufStr.length());
+        mOperator = 'o';
+        mNum = 0.0;
+        mPreOperands = 0.0;
+        mShowResultEdt.setText("");
     }
-
-    private void perform(Character operator) {
-        numberStr.delete(0, numberStr.length());
-        numtemp = num;
-        this.operator=operator;
-    }
-
-    private void calculate() {
-        if (operator == '+'){
-            num = numtemp + num;
-        }else if (operator == '-'){
-            num = numtemp - num;
-        }else if (operator == '*'){
-            num = numtemp * num;
-        }else if (operator == '/'){
-            num = numtemp / num;
-        }
-        numberStr.delete(0, numberStr.length());
-        showResult.setText("" + num);
-    }
-
 }
