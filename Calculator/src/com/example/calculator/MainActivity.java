0d9d9beb099c5d4c9b559ac1bcd353af131a50d0
chieyo_chen <chieyo_chen@askey.com.tw>
2015-07-27 14:33:43 +0800

    1.fix avoid '.' char cause double value analysis error. 2.fix calculate bug after equal calculation in doOperatorBtnClicked,doNumberBtnClicked and doBackspaceBtnClicked 3.hide action bar 4.add doOperatorBtnClicked function to have change operator ability 5.add calculation formula show ability

--- Calculator/src/com/example/calculator/MainActivity.java
+++ Calculator/src/com/example/calculator/MainActivity.java
@@ -6,24 +6,29 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends Activity {
 
     private EditText mShowResultEdt;
-
+    // Define calculator support operator list
+    private ArrayList<Character> mOperatorList = new ArrayList<Character>(Arrays.asList('+', '-', '*', '/'));
     private StringBuffer mNumberBufStr = null;
-    private Character mOperator = null;
-    private double mNum = 0.0;
-    private double mPreOperands = 0.0;
+    private StringBuffer mShowResultBufStr = null;
+    private Character mOperator = '=';
+    private double mPreOperand = 0.0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowResultEdt = (EditText) findViewById(R.id.result_id);
+        getActionBar().hide();
 
         // initial
         mNumberBufStr = new StringBuffer();
-        doDataReset();
+        mShowResultBufStr = new StringBuffer();
     }
 
     /**
@@ -31,14 +36,28 @@ public class MainActivity extends Activity {
      */
     public void doNumberBtnClicked(View v) {
         Button btn = (Button) v;
-        Character c = btn.getText().charAt(0);
+        Character numberChar = btn.getText().charAt(0);
+        //avoid '.' char cause double value analysis error.
+        if (numberChar == '.'){
+            if(mNumberBufStr.length()==0 || mNumberBufStr.toString().endsWith(numberChar.toString())){
+                return;
+            }
+        }
 
-        if (c == '.' && mNumberBufStr.length() == 0)
-            return;
+        // clear showResult String for mNumBufStr merge
+        if (mNumberBufStr.length() != 0) {
+            if (mShowResultBufStr.toString().endsWith(mNumberBufStr.toString()))
+                mShowResultBufStr.delete(mShowResultBufStr.length() - mNumberBufStr.length(),
+                        mShowResultBufStr.length());
+        } else {
+            // after Equal calculate then enter number action
+            if (mOperator == '=')
+                mShowResultBufStr.delete(0, mShowResultBufStr.length());
+        }
 
-        mNumberBufStr.append(c);
-        mNum = Double.valueOf(mNumberBufStr.toString()).doubleValue();
-        mShowResultEdt.setText(mNumberBufStr);
+        mNumberBufStr.append(numberChar);
+        mShowResultBufStr.append(mNumberBufStr.toString());
+        mShowResultEdt.setText(mShowResultBufStr.toString());
     }
 
     /**
@@ -62,51 +81,107 @@ public class MainActivity extends Activity {
      * Button OnClickListener do Operator button click logic
      */
     public void doOperatorBtnClicked(View v) {
-        if (mPreOperands != 0.0 && mPreOperands != 'o' && mNum != 0.0) {
-            mNum = calculator(mPreOperands, mOperator, mNum);
-        }
         Button btn = (Button) v;
-        this.mOperator = btn.getText().charAt(0);
-        mPreOperands = mNum;
-        mNumberBufStr.delete(0, mNumberBufStr.length());
+
+        if (mNumberBufStr.length() != 0) {
+            if (mOperator != '=') {
+                mPreOperand = calculator(mPreOperand, mOperator,
+                        Double.valueOf(mNumberBufStr.toString()).doubleValue());
+            } else {
+                mPreOperand = Double.valueOf(mNumberBufStr.toString()).doubleValue();
+            }
+
+            mOperator = btn.getText().charAt(0);
+
+            mNumberBufStr.delete(0, mNumberBufStr.length());
+            mShowResultBufStr.delete(0, mShowResultBufStr.length());
+            mShowResultBufStr.append(String.valueOf(mPreOperand).replace(".0", "") + mOperator);
+            mShowResultEdt.setText(mShowResultBufStr.toString());
+
+        } else {
+            if (mOperator != '=') {
+                // for change operator
+                if (mShowResultBufStr.toString().endsWith(mOperator.toString())) {
+                    mOperator = btn.getText().charAt(0);
+                    mShowResultBufStr.delete(mShowResultBufStr.length() - 1, mShowResultBufStr.length());
+                    mShowResultBufStr.append(mOperator.toString());
+                    mShowResultEdt.setText(mShowResultBufStr.toString());
+                }
+            } else {
+                // after Equal calculate
+                if (mShowResultBufStr.length() != 0) {
+                    mOperator = btn.getText().charAt(0);
+                    mShowResultBufStr.append(mOperator);
+                    mShowResultEdt.setText(mShowResultBufStr.toString());
+                }
+            }
+        }
+
     }
 
     /**
      * Button OnClickListener do Operator button click logic
      */
     public void doEqualBtnClicked(View v) {
-        mNum = calculator(mPreOperands, mOperator, mNum);
-        String ans = String.valueOf(mNum).replace(".0", "");
-        mOperator = 'o';
-        mNumberBufStr.delete(0, mNumberBufStr.length());
-        mShowResultEdt.setText("" + ans);
+        if (mNumberBufStr.length() != 0) {
+            if (mOperator != '=') {
+                Double postOperand = Double.valueOf(mNumberBufStr.toString()).doubleValue();
+                // handle divided by 0 problem
+                if (mOperator == '/' && postOperand == 0.0) {
+                    return;
+                } else {
+                    mPreOperand = calculator(mPreOperand, mOperator, postOperand);
+                }
+            } else {
+                mPreOperand = Double.valueOf(mNumberBufStr.toString()).doubleValue();
+            }
+
+            mNumberBufStr.delete(0, mNumberBufStr.length());
+            mShowResultBufStr.delete(0, mShowResultBufStr.length());
+            mShowResultBufStr.append(String.valueOf(mPreOperand).replace(".0", ""));
+            mShowResultEdt.setText(mShowResultBufStr.toString());
+
+        }
+        mOperator = '=';
     }
 
     /**
      * Button OnClickListener do clear button click logic
      */
     public void doClearBtnClicked(View v) {
-        doDataReset();
+        mNumberBufStr.delete(0, mNumberBufStr.length());
+        mShowResultBufStr.delete(0, mShowResultBufStr.length());
+        mShowResultEdt.setText(mShowResultBufStr.toString());
+        mOperator = '=';
+        mPreOperand = 0.0;
     }
 
     /**
      * Button OnClickListener do backspace button click logic
      */
     public void doBackspaceBtnClicked(View v) {
-        if (mNumberBufStr.length() > 0) {
-            mNumberBufStr.delete(mNumberBufStr.length() - 1, mNumberBufStr.length());
-            mShowResultEdt.setText(mNumberBufStr);
+        if (mShowResultBufStr.length() > 0) {
+            Character lastChar = mShowResultBufStr.toString().charAt(mShowResultBufStr.length() - 1);
+            // Judge last Character is number or operator
+            if (mOperatorList.contains(lastChar)) {
+                mOperator = '=';
+            } else {
+                if (mNumberBufStr.length() == 0) {
+                    if (Double.valueOf(mShowResultBufStr.toString()).doubleValue() == mPreOperand) {
+                        mNumberBufStr.append(mShowResultBufStr.toString());
+                        mNumberBufStr.delete(mNumberBufStr.length() - 1, mNumberBufStr.length());
+                    } else {
+                        // after Equal calculate then enter Backspace action
+                        mShowResultBufStr.delete(0, mShowResultBufStr.length());
+                        mShowResultEdt.setText(mShowResultBufStr.toString());
+                        return;
+                    }
+                } else {
+                    mNumberBufStr.delete(mNumberBufStr.length() - 1, mNumberBufStr.length());
+                }
+            }
+            mShowResultBufStr.delete(mShowResultBufStr.length() - 1, mShowResultBufStr.length());
+            mShowResultEdt.setText(mShowResultBufStr.toString());
         }
-    }
-
-    /**
-     * Initial data
-     */
-    private void doDataReset() {
-        mNumberBufStr.delete(0, mNumberBufStr.length());
-        mOperator = 'o';
-        mNum = 0.0;
-        mPreOperands = 0.0;
-        mShowResultEdt.setText("");
     }
 }
